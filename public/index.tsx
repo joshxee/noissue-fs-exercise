970b9d72f78de0185a4152174841bb6bfd551033
Josh Cleland <jbxcleland@gmail.com>
2026-04-21 10:01:33 +1200

    chore: remove dead UI components, unused deps, and isolate e2e from bun test runner

--- public/index.tsx
+++ public/index.tsx
@@ -1,14 +1,23 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom'
 import Checkout from './pages/Checkout'
 import OrderConfirmation from './pages/OrderConfirmation'
 import Store from './pages/Store'
 import './index.css'
 
+function ScrollToTop() {
+	const { pathname } = useLocation()
+	useEffect(() => {
+		window.scrollTo(0, 0)
+	}, [pathname])
+	return null
+}
+
 function App() {
 	return (
 		<BrowserRouter>
+			<ScrollToTop />
 			<Routes>
 				<Route path="/" element={<Checkout />} />
 				<Route path="/confirmation" element={<OrderConfirmation />} />
